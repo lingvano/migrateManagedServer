38f7f2ef5bbc41f1aa954f347c14d85bcf82b2da
Matthias <matthias.fischer@lingvano.com>
2021-09-10 17:56:30 +0200

    Simplify config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,18 +2,28 @@ import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const originConfig = {
-  originHost: process.env.ORIGIN_HOST || '',
-  originPort: process.env.ORIGIN_PORT || 22,
-  originUserName: process.env.ORIGIN_USER || '',
-  originPassword: process.env.ORIGIN_PASSWORD || '',
-  originFolder: process.env.ORIGIN_FOLDER || '',
-  originPath: process.env.ORIGIN_PATH || '/',
+const origin = {
+  host: process.env.ORIGIN_HOST || '',
+  port: process.env.ORIGIN_PORT || 22,
+  userName: process.env.ORIGIN_USER || '',
+  password: process.env.ORIGIN_PASSWORD || '',
+  folder: process.env.ORIGIN_FOLDER || '',
+  path: process.env.ORIGIN_PATH || '/',
+};
+
+const destination = {
+  host: process.env.DESTINATION_HOST || '',
+  port: process.env.DESTINATION_PORT || 22,
+  userName: process.env.DESTINATION_USER || '',
+  password: process.env.DESTINATION_PASSWORD || '',
+  folder: process.env.DESTINATION_FOLDER || '',
+  path: process.env.DESTINATION_PATH || '/',
 };
 
 const config = {
   environment: process.env.NODE_ENV || 'development',
-  ...originConfig,
+  origin,
+  destination,
   downloadsDir: path.join('./', 'downloads') + '/',
 };
 export default config;
