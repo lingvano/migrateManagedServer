2d5d5ed8978f9cfc0c860f1b18133c4367c26821
Matthias <matthias.fischer@lingvano.com>
2021-09-10 17:56:12 +0200

    Fix iteration
    object value needs to be in brackets to allow variables to be passed

--- src/lib/config.test.ts
+++ src/lib/config.test.ts
@@ -7,18 +7,18 @@ describe('Valid configuration set up', () => {
     expect(typeof config.environment).toEqual('string');
   });
 
-  const servers = ['origin', 'destination'];
+  const servers: ['origin', 'destination'] = ['origin', 'destination'];
   servers.map((server) => {
     maybe(`Added environment variables for ssh access to ${server} server`, () => {
-      expect(typeof config.server.host).toEqual('string');
-      expect(typeof config.server.password).toEqual('string');
-      expect(typeof config.server.port).toEqual('number');
-      expect(typeof config.server.userName).toEqual('string');
+      expect(typeof config[server].host).toEqual('string');
+      expect(typeof config[server].password).toEqual('string');
+      expect(typeof config[server].port).toEqual('number');
+      expect(typeof config[server].userName).toEqual('string');
 
-      expect(config.server.host.length).toBeGreaterThan(0);
-      expect(config.server.password.length).toBeGreaterThan(0);
-      expect(config.server.port).toBeGreaterThan(0);
-      expect(config.server.userName.length).toBeGreaterThan(0);
+      expect(config[server].host.length).toBeGreaterThan(0);
+      expect(config[server].password.length).toBeGreaterThan(0);
+      expect(config[server].port).toBeGreaterThan(0);
+      expect(config[server].userName.length).toBeGreaterThan(0);
     });
   });
 });
