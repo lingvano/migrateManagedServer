1118862645d5cbd749d389a5807abac43626234c
Matthias <matthias.fischer@lingvano.com>
2021-09-10 17:36:09 +0200

    add originPath to config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,13 +2,18 @@ import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const config = {
-  environment: process.env.NODE_ENV || 'development',
+const originConfig = {
   originHost: process.env.ORIGIN_HOST || '',
   originPort: process.env.ORIGIN_PORT || 22,
   originUserName: process.env.ORIGIN_USER || '',
   originPassword: process.env.ORIGIN_PASSWORD || '',
   originFolder: process.env.ORIGIN_FOLDER || '',
-  downloadsDir: path.join('./', 'downloads'),
+  originPath: process.env.ORIGIN_PATH || '/',
+};
+
+const config = {
+  environment: process.env.NODE_ENV || 'development',
+  ...originConfig,
+  downloadsDir: path.join('./', 'downloads') + '/',
 };
 export default config;
