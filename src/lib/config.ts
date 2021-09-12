d5c921caf3dd062f2bfdf4c2ac440dd0bc88aa78
Matthias <matthias.fischer@lingvano.com>
2021-09-12 16:31:46 +0200

    Use environment file based on NODE_ENV

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const origin = {
   host: process.env.ORIGIN_HOST || '',
